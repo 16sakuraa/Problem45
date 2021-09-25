aa584fc8ec54f6af9346bfe698000667fe545f2d
OAT <64010860@kmitl.ac.th>
2021-09-25 13:57:44 +0700

    Add project files.

---
+++ Problem45/45.1.cpp
@@ -0,0 +1,26 @@
+﻿//  45.จงเขียนฟังก์ชั่น factorial (Level 3)
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+
+
+int fac(int x)
+{
+	int sum=1,i;
+	for (i = 1; i <= x; i++)
+	{
+		sum *= i;
+	}
+
+	return sum;
+
+
+}
+
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	printf("%d",fac(x));
+	
+
+}
---
+++ Problem45/45.2.cpp
@@ -0,0 +1,26 @@
+﻿//  45.จงเขียนฟังก์ชั่น factorial (Level 3)
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+
+
+int fac(int x)
+{
+	int sum = 1, i;
+	for (i = x; i >= 1; i--)
+	{
+		sum *= i;
+	}
+
+	return sum;
+
+
+}
+
+int main()
+{
+	int x;
+	scanf("%d", &x);
+	printf("%d", fac(x));
+
+
+}
